4d79f822dac20f1fba278a59df1af8e03f2d8a8d
Emre Akadal <emreakadal@gmail.com>
2023-02-28 15:01:34 +0300

    Create contract.sol

---
+++ contract.sol
@@ -0,0 +1,31 @@
+// Istanbul BTC
+// Istanbul University Faculty of Economics
+// Blockchain Tecnologies and Innovation Center
+
+pragma solidity ^0.8.0;
+
+contract IstanbulBTCCertification {
+	
+	struct Event {
+
+		//
+
+	}
+
+	struct Certificate {
+
+		//
+
+	}
+
+	Event[] private events;
+
+	Certificate[] private certificates;
+
+	bytes32 public constant ADMIN = keccak256("ADMIN");
+    bytes32 public constant EVENT_CREATOR = keccak256("EVENT_CREATOR");
+    bytes32 public constant MINTER = keccak256("MINTER");
+
+
+
+}
